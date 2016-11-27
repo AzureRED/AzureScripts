cb6b65ea446dd95f2e2fa9f94642756af9b6ab1e
AzureRED <rdeseke9000@outlook.com>
2016-11-27 03:33:36 +0000

    recurssion search

--- jsonread.py
+++ jsonread.py
@@ -6,11 +6,53 @@ import os
 import shutil
 import re
 
+# Reading nesting Dictionaries
+def FindDict(Masterkey, Dkey):
+
+    Dvalue = None
+    print "FindDict:", Masterkey
+    for Masterkey in json:
+        print "###", Masterkey #isinjson(Masterkey)
+        #if Masterkey == (Masterkey)
+        #if isinjson()
+        #    if isinstance(Dvalue, list):
+        #        Dvalue = [ v.get(Dkey, default) if v else None for v in Dvalue]
+        #    else:
+        #        Dvalue = Dvalue.get(Dkey, default)
+        #else:
+        #    Dvalue = json.get(self, Dkey, default)
+        #if not Dvalue:
+        #    break;
+    return Dvalue
+    
+def getvalue(searchdict, Dkey):
+    fields = []  # start with blank
+    for key, value in searchdict.iteritems():
+        if key == Dkey:
+            fields.append(value) # if found at same level
+            print "key equals:", fields.append(value) 
+            
+        elif isinstance(value, dict):  # if in dictionary then parse sub-section
+            results = getvalue(value, Dkey)
+            print "results:", results
+            for result1 in results:
+                fields.append(result1)
+
+        elif isinstance(value, list):  # if in list then parse sub-section
+            for item in value:
+                print "Item:", item
+                if isinstance(item, dict):
+                    moreresults = getvalue(item, Dkey)
+                    print "moreresults:", moreresults
+                    for result2 in moreresults:
+                        fields.append(result2)
+    return fields
+    
 # A more robust way to find the keys:values
-def isinjson(jskey):
-    if jskey in Testjson.keys():
+def isinjson(tempjson, jskey):
+    if jskey in tempjson.keys():
         try:
-            return Testjson[jskey] 
+            return tempjson[jskey] 
         except:
             print "Error with Key Retrival", jskey
             exit(4)
@@ -20,17 +62,17 @@ def isinjson(jskey):
 def readjson(filename, localrun):
   try:
     with open(filename) as json_file:
-      global Testjson 
+      #global Testjson 
       Testjson = json.load(json_file)
     
     #pprint(Testjson)
     print("find the top level")
     print(Testjson.keys())
 
-    VMname = isinjson('name')
-    VMlocation = isinjson('location')
-    VMsub = isinjson('id')
-    VMsize = isinjson('hardwareProfile')
+    VMname = isinjson(Testjson, 'name')
+    VMlocation = isinjson(Testjson, 'location')
+    VMsub = isinjson(Testjson,'id')
+    VMsize = isinjson(Testjson,'hardwareProfile')
     
     print "VM name :",VMname
     print "Location :",VMlocation
@@ -50,13 +92,20 @@ def readjson(filename, localrun):
             print "Error reading ID line of VM " + VMname + " JSON."
             exit(5)
   
-    VMstorage = isinjson('storageProfile')
-    print VMstorage, "StorageProfile"
-    VMOSdisk = VMstorage.get('osDisk')
-    print VMOSdisk, "Disk"
-    print VMstorage.get('osType'), "type"
-    VMOSuri = VMOSdisk.get('vhd')
-    print VMOSuri
+    print "## start ##"
+    print "Output :", getvalue(Testjson, 'uri')
+    Networksub = getvalue(Testjson, 'networkProfile')
+    print "Output :", getvalue(Testjson, 'id')
+  
+    #print FindDict('storageProfile', 'url')
+  
+    #VMstorage = isinjson('storageProfile')
+    #print VMstorage, "StorageProfile"
+    #VMOSdisk = VMstorage.get('osDisk')
+    #print VMOSdisk, "Disk"
+    #print VMstorage.get('osType'), "type"
+    #VMOSuri = VMOSdisk.get('vhd')
+    #print VMOSuri
 
     
     
