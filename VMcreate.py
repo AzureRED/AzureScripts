b40cc2f9965073e0da5e020d5ebf010f691ee5f5
AzureRED <rdeseke9000@outlook.com>
2017-01-01 05:52:01 +0000

    Added propper verbose option.

--- VMcreate.py
+++ VMcreate.py
@@ -37,15 +37,14 @@ def JsonValue(searchdict, Dkey):
 
      
 ### Is is to print to screen    
-def MePrint(thestring):
+def MePrint(prestring, poststring):
     if printit:
-        print (thestring) #tee to stderr
+        print (prestring, poststring) #tee to stderr
     
     
 ### List Helper for JSON parsing  
 ### pulls the first element from a list  
 def Curtailist(choplist):  
-    print "CHOPLIST ", choplist
     if len(choplist) > 0:
         choplist = choplist[0]
         if not choplist:
@@ -54,10 +53,10 @@ def Curtailist(choplist):
         else:
             return choplist 
     elif len(choplist) == 0:   
-        print "CHOPLIST zero", choplist
+        MePrint("CHOPLIST zero", str(choplist))
         return choplist
     else:
-        print "Nothing in List", choplist
+        MePrint("Nothing in List", "[ ]")
         return choplist
 
         
@@ -66,22 +65,22 @@ def Jsonparse(Testjson):
     VMbuild = ''  # the start of the VM create command     
     # these calls will output lists and more then one element 
     VMname = ''.join(Curtailist(JsonValue(Testjson, 'name')))  # top level
-    print "VM name :" , VMname 
+    MePrint("VM name :" , VMname) 
     VMlocation = ''.join(Curtailist(JsonValue(Testjson, 'location')))
-    print "Location :", VMlocation   
+    MePrint("Location :", VMlocation)   
     VMsize = ''.join(Curtailist(JsonValue(Testjson, 'vmSize')))
-    print "Size: ", VMsize    
+    MePrint("Size: ", VMsize)    
     VMostype = ''.join(Curtailist(JsonValue(Testjson, 'osType')))
-    print "VMostype :", VMostype
+    MePrint("VMostype :", VMostype)
     VMsub = ''.join(Curtailist(JsonValue(Testjson, 'id')))
-    print "VMID :", VMsub
+    MePrint("VMID :", VMsub)
     
     # Pull apart the VMID to its' components
     if VMsub.find("/") <> -1:
         VMIDlist = list(VMsub.split("/"))
         if VMIDlist[1] == "subscriptions":
-            print VMIDlist[1], ":",  VMIDlist[2]  # subscription and subscription ID
-            print VMIDlist[3], ":",  VMIDlist[4]  # resourceGroup
+            MePrint (VMIDlist[1],  VMIDlist[2])  # subscription and subscription ID
+            MePrint(VMIDlist[3],  VMIDlist[4])  # resourceGroup
             VMsubscription = VMIDlist[2]
             VMresourcegrp = VMIDlist[4]
         else:
@@ -89,7 +88,7 @@ def Jsonparse(Testjson):
     else:
         sys.exit("Error reading ID line of VM.  Missing delimiters. " + VMname + " JSON.") 
     
-    print "## start Networking ## " + VMresourcegrp
+    MePrint ("## start Networking ## ", VMresourcegrp)
     Networkstub = Curtailist(JsonValue(Testjson, 'networkInterfaces'))  
     if not Networkstub:
         sys.exit("Error Missing Networking Interfaces")
@@ -97,14 +96,14 @@ def Jsonparse(Testjson):
         NICliststr = ''        
         for NIC in Networkstub:
             VMnetlist = ''.join(JsonValue(NIC, "id")) 
-            print "VMnetlist :", VMnetlist
+            MePrint ("VMnetlist :", VMnetlist)
             if VMnetlist.find("/") <> -1:
                 VMnetlist = list(VMnetlist.split("/"))
                 if VMnetlist[7] == "networkInterfaces":  #is the networkInterface there?
-                    print "network NIC name :", VMnetlist[8] 
+                    MePrint ("network NIC name :", VMnetlist[8]) 
                     NICliststr = NICliststr + VMnetlist[8] + ', '
         NICliststr = NICliststr[:-2]  # Take the last comma off the end 
-        print "NIC list String :", NICliststr
+        MePrint("NIC list String :", NICliststr)
      
     # OS disk location
     VMosdiskset = Curtailist(JsonValue(Testjson, 'osDisk'))
@@ -113,23 +112,22 @@ def Jsonparse(Testjson):
         VMosdisk = '"' + VMosdisk + '"'
         if VMosdisk.find("/") <> -1:
             VMstoragegrp = re.split('/|\.', VMosdisk)
-            print "Storage Group : ", VMstoragegrp[2]
+            MePrint ("Storage Group : ", VMstoragegrp[2])
             VMstoragegrp = ''.join(VMstoragegrp[2])
     else:
         sys.exit(" Missing OS Disk, HALTING. \n")
     
     # Datadisk, possible list
     dataDisks = Curtailist(JsonValue(Testjson, 'dataDisks'))
-    MePrint(str("DATADISKS " + dataDisks))
     if dataDisks:
-        print "DATA DISKS FOUND: ", dataDisks
+        MePrint ("DATA DISKS FOUND: ", dataDisks)
         Datalist = ''
         for DDisks in dataDisks:
             DataDStr = '"' + ''.join(JsonValue(DDisks, "uri")) 
-            print "Datadisk :", DataDStr
+            MePrint ("Datadisk :", DDisks)
             Datalist = Datalist + DataDStr + '"' + ", "
         Datalist = Datalist[:-2] # Take the last comma off the end
-        print "Datalist :", Datalist
+        MePrint ("DataDisk List:", Datalist)
         
     # Username and Passord/Key is built in to the OS disk and not used
     # Build the create vm command
@@ -163,7 +161,7 @@ def main():
 # -v verbose output.
 # By default the script will just print the command.
 #
-# $> jsonread.py [-r,-h] <jsonfile> 
+# $> jsonread.py [-r,-h,-v] <jsonfile> 
 """
                 print Helpstr
                 sys.exit(0)
@@ -176,9 +174,12 @@ def main():
                     with open(arg) as json_file:
                         VMcreate = Jsonparse(json.load(json_file))
                         if runcommand:
+                            print '\nRunning Command\n'
                             print VMcreate
+                            ### run comm VM create
                         else:
-                            print "No run :: " + VMcreate
+                            print "\nPaste this command into the Azure CLI to run the create for the VHD.\n"                        
+                            print VMcreate
                 except ValueError:
                         sys.exit(" Could not parse JSON file. \n Make sure that the JSON is properly formated.\n")                      
             else:
